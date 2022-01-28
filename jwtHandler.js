1a0c76d30ffb3630ad692cca40bc7a4890a0c46a
Mateusz Kaczynski <mateusz.kaczynski@checkd.media>
2022-01-28 15:18:03 +0000

    asd

--- jwtHandler.js
+++ jwtHandler.js
@@ -3,24 +3,17 @@ const axios = require('axios')
 const jwkToPem = require('jwk-to-pem');
 
 let jwks = undefined
-
 async function verify(token) {
     const decoded = jwt.decode(token, { complete: true });
     if (!jwks) {
         jwks = {}
-        const { data } = await axios.get("https://cognito-idp.eu-central-1.amazonaws.com/{cognitoPoolId}/.well-known/jwks.json")
+        const { data } = await axios.get("https://cognito-idp.{region}.amazonaws.com/{cognitoPoolID}/.well-known/jwks.json")
         data.keys.forEach(jwk => {
             jwks[jwk.kid] = jwk;
         })
     }
-    try {
-        const pem = jwkToPem(jwks[decoded.header.kid]);
-        const result = await check(token, pem)
-        return result
-    } catch (e) {
-        console.log(e)
-    }
-    return jwt.decode(token, { complete: true });
+    const pem = jwkToPem(jwks[decoded.header.kid]);
+    return await check(token, pem)
 }
 
 async function check(token, pem) {
